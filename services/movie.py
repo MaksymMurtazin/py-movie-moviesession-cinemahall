c90ed8518818a7992f226ca9b9951e0faafebbfa
Maksym Murtazin <max.murtazin2006@gmail.com>
2026-08-20 14:07:46 +0300

    changed .add() to .set()

--- services/movie.py
+++ services/movie.py
@@ -39,6 +39,6 @@ def create_movie(
         description=movie_description
     )
     if genres_ids:
-        new_movie.genres.add(*genres_ids)
+        new_movie.genres.set(genres_ids)
     if actors_ids:
-        new_movie.actors.add(*actors_ids)
+        new_movie.actors.set(actors_ids)
